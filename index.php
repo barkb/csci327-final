203c3e183d736f0941942ec895b24514824f9433
Ben Barkett <barkettbb@mx.lakeforest.edu>
2021-05-01 20:59:20 +0000

    Updated index.php to include project requirements

--- index.php
+++ index.php
@@ -1,5 +1,49 @@
 <?php
-echo "Hello, World!";
+echo "CSCI 327 Final Project";
+echo "OK Max and Ian. This is the homepage for our final project.\n
+Let's keep the basic requirements of the project on this page as we're working
+on it so that we have something easy to refer back to.\n\n";
 
-echo "this is ben's commit";
+echo "Prompt: \n";
+echo "You are to develop a menu driven application system for the public library system database. The following are menus to be developed. Try to implement all applications described below. If you want you may add more functions and menus. If you miss a task mention which one. In this case if you have implemented a task with a similar functionality mention it.\n\n";
+
+echo "1. Main Menu: \n";
+echo "	Customer functions (ask for card number/userID, and then show sub-menu).\n
+	Administrative functions (ask for ID and password, and then show submenu).\n
+	Quit.\n\n";
+
+echo "Customer Functions Submenu: \n";
+echo "	Search a movie by category, title, or director name.\n
+	Movie checkout\n
+	Movie return\n
+	Movie reserve\n
+	Compute fine for a movie copy checked out by a customer based on the current date\n
+	Print the list of movies reserved by a customer and their status\n
+	Print the movie ids and movie titles of movies directed by a director\n
+	Quit/Logout\n\n";
+
+echo "Administrative Functions Menu: \n";
+echo "	Add a movie copy\n
+	Search a movie and check the status of its copies\n
+	Add new customer\n
+	Print store information (name and location)\n
+	Print top 10 most frequent renters in a store and the number of movies each has rented\n
+	Print top 10 most rented movies in a store\n
+	Print top 10 most popular movies of the year\n
+	Find the average fine paid per customer\n
+	Quit/logout\n\n";
+
+echo "What is expected in your submission: \n";
+echo "	Description of the implementation, problems faced.\n
+	The sql commands that create your tables\n
+	The sql commands that populate your tables\n
+	The php source code\n
+	A few screenshots of the major screens\n
+	Put 1,2 in a single docx or pdf file and 3,4 as a text file. The php folder should be given as one .zip\n\n";
+
+echo "Apart from the group submission, there should be: \n";
+echo "	An individual submission that states what the different group members including you did for your project.\n\n";
+
+echo "DUE ON TUESDAY, MAY 11 @ 1:30PM";
+echo "GL HF";
 ?>
