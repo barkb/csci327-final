510cd274f853266daa78929871d781fdee2e32cd
Ben Barkett <barkettbb@mx.lakeforest.edu>
2021-05-01 21:15:54 +0000

    Alright this should do it

--- index.php
+++ index.php
@@ -1,49 +1,49 @@
 <?php
-echo("CSCI 327 Final Project\r\n");
-echo("OK Max and Ian. This is the homepage for our final project.\n
+echo("CSCI 327 Final Project<br/>");
+echo("OK Max and Ian. This is the homepage for our final project.<br/>
 Let's keep the basic requirements of the project on this page as we're working
-on it so that we have something easy to refer back to.\n\n");
+on it so that we have something easy to refer back to.<br/><br/>");
 
-echo("Prompt: \n");
-echo("You are to develop a menu driven application system for the public library system database. The following are menus to be developed. Try to implement all applications described below. If you want you may add more functions and menus. If you miss a task mention which one. In this case if you have implemented a task with a similar functionality mention it.\n\n");
+echo("Prompt: <br/>");
+echo("You are to develop a menu driven application system for the public library system database. The following are menus to be developed. Try to implement all applications described below. If you want you may add more functions and menus. If you miss a task mention which one. In this case if you have implemented a task with a similar functionality mention it.<br/><br/>");
 
-echo("1. Main Menu: \n");
-echo("	Customer functions (ask for card number/userID, and then show sub-menu).\n
-	Administrative functions (ask for ID and password, and then show submenu).\n
-	Quit.\n\n");
+echo("1. Main Menu: <br/>");
+echo("	Customer functions (ask for card number/userID, and then show sub-menu).<br/>
+	Administrative functions (ask for ID and password, and then show submenu).<br/>
+	Quit.<br/><br/>");
 
-echo("Customer Functions Submenu: \n");
-echo("	Search a movie by category, title, or director name.\n
-	Movie checkout\n
-	Movie return\n
-	Movie reserve\n
-	Compute fine for a movie copy checked out by a customer based on the current date\n
-	Print the list of movies reserved by a customer and their status\n
-	Print the movie ids and movie titles of movies directed by a director\n
-	Quit/Logout\n\n");
+echo("Customer Functions Submenu: <br/>");
+echo("	Search a movie by category, title, or director name.<br/>
+	Movie checkout<br/>
+	Movie return<br/>
+	Movie reserve<br/>
+	Compute fine for a movie copy checked out by a customer based on the current date<br/>
+	Print the list of movies reserved by a customer and their status<br/>
+	Print the movie ids and movie titles of movies directed by a director<br/>
+	Quit/Logout<br/><br/>");
 
-echo("Administrative Functions Menu: \n");
-echo("	Add a movie copy\n
-	Search a movie and check the status of its copies\n
-	Add new customer\n
-	Print store information (name and location)\n
-	Print top 10 most frequent renters in a store and the number of movies each has rented\n
-	Print top 10 most rented movies in a store\n
-	Print top 10 most popular movies of the year\n
-	Find the average fine paid per customer\n
-	Quit/logout\n\n");
+echo("Administrative Functions Menu: <br/>");
+echo("	Add a movie copy<br/>
+	Search a movie and check the status of its copies<br/>
+	Add new customer<br/>
+	Print store information (name and location)<br/>
+	Print top 10 most frequent renters in a store and the number of movies each has rented<br/>
+	Print top 10 most rented movies in a store<br/>
+	Print top 10 most popular movies of the year<br/>
+	Find the average fine paid per customer<br/>
+	Quit/logout<br/><br/>");
 
-echo("What is expected in your submission: \n");
-echo("	Description of the implementation, problems faced.\n
-	The sql commands that create your tables\n
-	The sql commands that populate your tables\n
-	The php source code\n
-	A few screenshots of the major screens\n
-	Put 1,2 in a single docx or pdf file and 3,4 as a text file. The php folder should be given as one .zip\n\n");
+echo("What is expected in your submission: <br/>");
+echo("	Description of the implementation, problems faced.<br/>
+	The sql commands that create your tables<br/>
+	The sql commands that populate your tables<br/>
+	The php source code<br/>
+	A few screenshots of the major screens</br>
+	Put 1,2 in a single docx or pdf file and 3,4 as a text file. The php folder should be given as one .zip<br/><br/>");
 
-echo("Apart from the group submission, there should be: \n");
-echo("	An individual submission that states what the different group members including you did for your project.\n\n");
+echo("Apart from the group submission, there should be: <br/>");
+echo("	An individual submission that states what the different group members including you did for your project.<br/><br/>");
 
-echo("DUE ON TUESDAY, MAY 11 @ 1:30PM");
+echo("DUE ON TUESDAY, MAY 11 @ 1:30PM<br/><br/><br/>");
 echo("GL HF");
 ?>
