e814ca397b9a6e7eee81eef6ce029f1b3123b719
IanStew <ianstewsmail@gmail.com>
2021-05-11 13:25:59 -0500

    created custLogin

---
+++ custLogin.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+
+$servername ="localhost";
+$username = "barkb";
+$password = "csci327";
+$dbname = "videostore";
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+if ($conn -> connect_error){
+	die("Connection failed: " . $conn -> connect_error);
+}
+else{
+	     $userCust = $_POST["Custid"];
+        $passwordCust = $_POST["CustPass"];
+
+	$sql = "SELECT * FROM Member ";
+	$sql = $sql . "WHERE Username='".$userCust."' AND Passwd='".$passwordCust."'";
+
+
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0){
+		$row = $result->fetch_assoc();
+
+		$_SESSION['memberID'] = $row['MemberID'];
+		$_SESSION['username'] = $row['Username'];
+		$_SESSION['name'] = $row['Name'];
+		$_SESSION['address'] = $row['Address'];
+		$_SESSION['phone'] = $row['Phone'];
+		$_SESSION['storeNo'] = $row['StoreNo'];
+
+		echo $_SESSION['name'];
+		header("Location: custmenu.php");
+
+        }
+        else
+        {
+        	echo "error retry password";
+        	echo "<input type='reset' value='Reset' onClick='window.location.reload()'/>";
+
+	}
+}
