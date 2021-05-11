c5d179b547736ac7fb7ddbe2ac0d335360c87696
Ianstew <74681665+Ianstew@users.noreply.github.com>
2021-05-10 22:02:29 -0500

    Create Menu.php

---
+++ Menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head><title>Welcome to the Video Store</title></head>
+
+<body>
+<h1> Video Stroy</h1>
+<h3> Login</h3>
+<form action= "member.php"method="POST">
+	Username: <input type="text" name= "userid">
+	<br>
+	Password: <input type= "password" name= "password">
+	<br>
+</form>
+
+</body>
+</html>
