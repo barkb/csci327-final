62480543c8637cfc08638a916b200a79c47fe7fd
IanStew <ianstewsmail@gmail.com>
2021-05-10 23:53:40 -0500

    update

--- Welcome.php
+++ Welcome.php
@@ -6,12 +6,12 @@
 <body>
 <h1> Video Stroy</h1>
 <h3> Login</h3>
-<form action= "Menu.php"method="POST">
+<form action="Menu.php"method="POST">
 	Username: <input type="text" name= "userid">
 	<br>
 	Password: <input type= "password" name= "password">
 	<br><br>
-
+  <button type="submit" name="login" value="login">login</button>
 </form>
 
 </body>
