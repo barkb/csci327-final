27a7e6b3d4ba55717a9d16cc302ab8b58f5a340a
Ben Barkett <barkettbb@mx.lakeforest.edu>
2021-05-11 03:06:50 +0000

    continuing work on custmenu

--- adminmenu.php
+++ adminmenu.php
@@ -1,7 +1,23 @@
 <?php
-require 'init.php'; //Database connection and other required classes.
 
+//INIT file loads resources needed by pages in our project
 
+/*
+ * DATABASE CONNECTION
+ */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'barkb');
+define('DB_PASSWORD', 'csci327');
+define('DB_DATABASE', 'videostore');
+
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+if ($mysqli->connect_errno) {
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	exit;
+}
+else {
+	echo "success";
+}
 ?>
 
 <!doctype html>
@@ -13,19 +29,61 @@ require 'init.php'; //Database connection and other required classes.
 <title>Video Store Admin</title>
 <link rel="stylesheet" href="adminmenu.css"/>
 
+<form>
+
 <body>
 	<div class="container">
 		<h1>Hello administrator</h1>
-		<button>Add movie copy</button>
+				<button>Add movie copy</button>
 		<input value="Search for movie" ></input>
 		<button>Add new customer</button>
-		<button>Print store information</button>
+		<form action="adminmenu.php" method="GET">
+			<input type="submit" value="Print store info" name="info"/>
+		</form>
 		<button>print top 10 most frequest renters in a store</button>
 		<button>print top 10 most rented movies in a store</button>
 		<button>print top 10 most popular movies of the year</button>
 		<button>Average fine per customer</button>
 		<button>Log Out</button>
 	</div>
+	<?php
+		if(isset($_GET['info'])){
+			$sql = 'SELECT * FROM Employee';
+			getresult($sql);
+		}
+		elseif(isset($_GET['toprenters'])){
+			$sql = '';
+			getresult($sql);
+		}
+		elseif(isset($_GET['toprented'])){
+			$sql = '';
+			getresult($sql);
+
+		}
+		elseif(isset($_GET['yearly'])){
+			$sql = '';
+			getresult($sql);
+		}
+		elseif(isset($_GET['avgfine'])){
+			$sql = '';
+			getresult($sql);		
+		}
+	
+			
+		function getresult($sql) {
+			global $mysqli;
+			$results = $mysqli->query($sql);
+			#echo($mysqli->error);
+			$fields = $results->fetch_fields();
+			foreach ($fields as $field){
+				echo $field->name;
+			}
+			
+		}
+
+		$mysqli->close();
+
+	?>
 
 </body>
 </html>
--- custmenu.php
+++ custmenu.php
@@ -8,10 +8,28 @@ require 'init.php'; //Database connection and other required classes.
 <!DOCTYPE html>
 <html>
 	<head>
+		<title>Video Store Customer</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="adminmenu.css"/>
 
 	</head>
 
 	<body>
 		<h1>Welcome Customer</h1>
+
+		<form action="moviesearch.php" method="POST">
+			<input type="hidden" name="task" value="save">
+			<input type="hidden" name="entry_id" value="<?=$entry_id?>">
+			<!-- Movie search box -->
+			<h4>Search by Movie Title, Director, or Genre</h4>
+			<label for="Title">Title: </label>
+			<input type="text" name="Title" value="<?= $Title?>"><br>
+			<label for="Director">Director: </label>
+			<input type="text" name="Director" value="<?= $Director?>"><br>
+			<label for="Genre">Genre: </label>
+			<input type="text" name="Genre" value="<?= $Genre?>"><br>
+			<input type="submit" value="submit">
+		</form>
+
 	</body>
 </html>
