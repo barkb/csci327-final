c0710014d9f266bfba13adeea8a2c2b91b34900b
IanStew <ianstewsmail@gmail.com>
2021-05-10 22:48:23 -0500

    add Welcom.php

---
+++ Welcome.php
@@ -0,0 +1,18 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head><title>Welcome to the Video Store</title></head>
+
+<body>
+<h1> Video Stroy</h1>
+<h3> Login</h3>
+<form action= "Menu.php"method="POST">
+	Username: <input type="text" name= "userid">
+	<br>
+	Password: <input type= "password" name= "password">
+	<br><br>
+
+</form>
+
+</body>
+</html>
